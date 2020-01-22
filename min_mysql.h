dd22a9f8e0defe15d4fdab853aa750ab0b6829ba
Roy Bellingan <roy@tech.chameleon.ad>
2020-01-22 14:21:00 +0000

    new const

--- min_mysql.h
+++ min_mysql.h
@@ -6,11 +6,12 @@
 
 //Those variable are shared in many places, order of initialization is important!
 //Inline will avoid to have multiple copy, and enforces having a single one
-inline const QString    mysqlDateFormat = "yyyy-MM-dd";
-inline const QString    SQL_NULL        = "NULL";
-inline const QByteArray BSQL_NULL       = "NULL";
-inline const QByteArray BZero           = "0";
-inline const QString    Zero            = "0";
+inline const QString    mysqlDateFormat     = "yyyy-MM-dd";
+inline const QString    mysqlDateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+inline const QString    SQL_NULL            = "NULL";
+inline const QByteArray BSQL_NULL           = "NULL";
+inline const QByteArray BZero               = "0";
+inline const QString    Zero                = "0";
 
 QString base64this(const char* param);
 QString base64this(const QByteArray& param);
