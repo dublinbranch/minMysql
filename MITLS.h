bc9c21fcd16b8efe3830dc7326056d6d96c6a3cb
Roy Bellingan <roy@tech.techadsmedia.com>
2020-09-14 15:52:56 +0100

    incastrim

--- MITLS.h
+++ MITLS.h
@@ -40,41 +40,47 @@ class mi_tls_repository {
       protected:
 	void store(uintptr_t instance, T value) {
 		if (!repository) { //check if the application has been already terminated, and we have an out of order destruction
-			return;
+			repository = new std::unordered_map<uintptr_t, T>();
 		}
 		repository->operator[](instance) = value;
 	}
 
 	T load(uintptr_t instance) const {
 		if (!repository) { //check if the application has been already terminated, and we have an out of order destruction
-			return T();
+			repository = new std::unordered_map<uintptr_t, T>();
 		}
 		return repository->operator[](instance);
 	}
 
 	void remove(uintptr_t instance) {
 		if (!repository) { //check if the application has been already terminated, and we have an out of order destruction
-			return;
+			repository = new std::unordered_map<uintptr_t, T>();
 		}
 		if (repository->find(instance) != repository->cend()) {
 			repository->erase(instance);
 		}
 	}
 
+	//This will be called ONLY for the first thread
 	mi_tls_repository() {
 		if (!repository) {
 			repository = new std::unordered_map<uintptr_t, T>();
 		}
 	}
 
+	//again this has no access to all the various thread local storage
+	//TODO add
 	~mi_tls_repository() {
-		delete (repository);
-		repository = nullptr;
+		if (repository) {
+			delete (repository);
+			repository = nullptr;
+		}
 	}
 
       private:
 	//Key = memory location of the INSTANCE
 	//Value = what you want to store
 	//This is manual because thread_local are auto freed, but free order can be wrong! so when the DB closes (and remove the conn) we are in the wrong place
+	//Therefore this will leak memory, once you close the program, so is 100% irrelevant
 	inline static thread_local std::unordered_map<uintptr_t, T>* repository = nullptr;
 };
